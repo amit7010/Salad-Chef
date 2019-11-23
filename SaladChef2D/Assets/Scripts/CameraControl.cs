ef899ac2dbe8366f92f8c56dc34e7a4732f7e7a1
Amit Singh <amitkumar7010@gmail.com>
2019-11-23 23:33:12 +0530

    Added Pause Menu and Scripts improvement

---
+++ SaladChef2D/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaladChef2D.UI
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraControl : MonoBehaviour
+    {
+        #region Variables
+
+        //Lit of all players
+        public List<Transform> players;
+
+        public Vector3 offset;
+        public float smoothTime = 0.2f;
+
+        private Vector3 velocity;
+
+        #endregion
+
+        private void LateUpdate()
+        {
+            if (players.Count == 0)
+                return;
+
+            MoveCamera();
+
+        }
+
+        /// <summary>
+        /// Function to move camera
+        /// </summary>
+        private void MoveCamera()
+        {
+            Vector3 centerPoint = GetCenterPoint();
+
+            Vector3 newPosition = centerPoint + offset;
+
+            //Function to smoothout movement of camera
+            transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+        }
+
+        /// <summary>
+        /// Function to get centre points through bounds
+        /// </summary>
+        /// <returns></returns>
+        private Vector3 GetCenterPoint()
+        {
+            if (players.Count == 1)
+            {
+                return players[0].position;
+            }
+
+            var bounds = new Bounds(players[0].position, Vector3.zero);
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                bounds.Encapsulate(players[i].position);
+            }
+
+            return bounds.center;
+        }
+    }
+}
---
+++ SaladChef2D/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaladChef2D.UI
+{
+    public class MenuControl : MonoBehaviour
+    {
+        public static bool isGamePaused = false;
+
+        public GameObject pauseMenuUI;
+
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                if(isGamePaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Function to resume the paused game
+        /// </summary>
+        private void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            isGamePaused = false;
+        }
+
+        /// <summary>
+        /// Function to pause game
+        /// </summary>
+        private void Pause()
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            isGamePaused = true;
+        }
+    }
+}
